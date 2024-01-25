4736e814d67b892e5bbaf122822fbe446d21cc6d
AnaghaCS <anaghasasidharan38@gmail.com>
2024-01-25 15:56:09 +0530

    create signup paga and functionality

---
+++ basics/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    
+    path('',views.index,name="index"),
+    path('signup',views.signup,name="signup"),
+    path('logins',views.logins,name="logins"),
+    path('welcome_page',views.welcome_page,name="welcome_page")
+
+]
--- basics/views.py
+++ basics/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def signup(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        myuser=User.objects.create_user(username,email,password)
+        myuser.save()
+        return redirect('logins')
+    return render(request,'signup.html')
+
+def logins(request):
+    return render(request,'logins.html')
+
+def welcome_page(request):
+    return render(request,'welcome_page.html')
